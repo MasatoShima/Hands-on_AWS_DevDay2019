94241cdfca56b10f16c69f37e00955e90a2d42b8
MasatoShima <masato.shima@icloud.com>
2019-10-07 02:14:51 +0900

    新規作成

---
+++ AWS_Chalice/helloworld/app.py
@@ -0,0 +1,95 @@
+from chalice import Chalice
+from chalice import Response
+from chalice import CORSConfig
+from chalice import BadRequestError
+from chalice import Rate
+from datetime import datetime
+
+
+app = Chalice(app_name="chalice-hands-on-helloworld")
+app.debug = True
+
+CITIES_TO_STATE = {
+    "seattle": "WA",
+    "portland": "OR",
+}
+
+cors_config = CORSConfig(
+    allow_origin="https://foo.example.com",
+    allow_headers=["X-Special-Header"],
+    max_age=600,
+    expose_headers=["X-Special-Header"],
+    allow_credentials=True
+)
+
+
+@app.route('/')
+def index():
+    return {"chalice": "hands-on"}
+
+
+@app.route("/cities/{city}")
+def state_of_city(city):
+    try:
+        city = CITIES_TO_STATE[city]
+
+        return Response(
+            body=city,
+            status_code=200,
+            headers={
+                "Content-Type": "text/plain"
+            }
+        )
+
+    except KeyError:
+        raise BadRequestError(
+            f"Unknown city '{city}', valid choices are: {', '.join(CITIES_TO_STATE.keys())}"
+        )
+
+
+@app.route("/resource/{value}", methods=["PUT"])
+def put_test(value):
+    return {"value": value}
+
+
+# 下の method (myview_post, myview_put) と競合するため, コメントアウト
+# @app.route("/myview", methods=["POST", "PUT"])
+# def myview():
+#     return {"message": "Hello!"}
+
+
+@app.route("/myview", methods=["POST"])
+def myview_post():
+    return {"message": "Method is POST"}
+
+
+@app.route("/myview", methods=["PUT"])
+def myview_put():
+    return {"message": "Method is PUT"}
+
+
+@app.route("/introspect")
+def introspect():
+    return app.current_request.to_dict()
+
+
+@app.route("/supports-cors", methods=["PUT"], cors=True)
+def supports_cors():
+    return {}
+
+
+@app.route("/custom_cors", methods=["GET"], cors=cors_config)
+def supports_custom_cors():
+    return {'cors': True}
+
+
+@app.schedule(Rate(1, unit=Rate.MINUTES))
+def periodic_task(event):
+    launch_time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+    # CloudWatch Logs にログを出力
+    print(f"function_launched: {launch_time}")
+
+    return {"function_launched": launch_time}
+
+# End
---
+++ AWS_Chalice/s3upload/app.py
@@ -0,0 +1,52 @@
+from chalice import Chalice
+import boto3
+
+BUCKET_NAME = "chalice-handson-masato-shima-20191003"
+
+S3 = boto3.client("s3")
+
+app = Chalice(app_name="chalice-hands-on-s3upload")
+
+
+@app.route(
+    "/upload_via_api",
+    methods=["PUT"],
+    content_types=["application/octet-stream"]
+)
+def upload_via_api():
+    # 今回はシンプルに S3 上のオブジェクト名を固定します
+    key = 'uploaded_via_api'
+
+    # クライアントからのリクエストボディ
+    body = app.current_request.raw_body
+
+    result = S3.put_object(
+        Body=body,
+        Bucket=BUCKET_NAME,
+        Key=key
+    )
+
+    return {"result": result}
+
+
+@app.route("/presigned_url", methods=["GET"])
+def presigned_url():
+    # シンプルにするためオブジェクト名は固定します
+    key = "uploaded_by_client"
+
+    # 10分間有効な署名付き URL を生成
+    expires_in = 600
+
+    url = S3.generate_presigned_url(
+        ClientMethod="put_object",
+        Params={"Bucket": BUCKET_NAME, "Key": key},
+        ExpiresIn=expires_in,
+        HttpMethod="PUT"
+    )
+
+    return {
+        "presigned_url": url,
+        "expires_in": expires_in
+    }
+
+# End
